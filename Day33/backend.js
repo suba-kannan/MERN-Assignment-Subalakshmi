ef53baea6a2eeb636dcc160d30d858b14b5f3453
suba-kannan <subakannan1409@gmail.com>
2026-04-09 11:47:12 +0530

    Day 33 assignment completed

---
+++ Day33/backend.js
@@ -0,0 +1,59 @@
+// 1. What is Node.js?
+
+// Node.js is a js runtime environment that allows you to run js outside the browser mainly on server side.
+// Node.js lets you use js to build backend applications(ServerResponse,API)
+// It is an object oriented(everything stored in an object)
+// It is non-blocking I/O.
+// It is biuld on chrome's v8 engine
+// It is good fot fast and scalable apps.
+
+
+// 2. Is Node.js a framework?
+
+// Node.js is not a framework. It is a runtime environment. 
+// Node.js have express framework build on top of it to provide structure and make code easy.
+
+
+// 3. What is V8 Engine?
+
+// v8 engine is a js engine developed by google.
+// It converts js code into machine code.
+
+
+// 4. What is NPM?
+
+// NPM is a node package manager.
+// It is used to install and manage packages.
+// Ex: npm install express
+
+
+// 5. What are modules in Node.js?
+
+// Modules are reusable pieces of code
+// 1. Core modules - built-in(fs, http, path, os)
+// 2. Local modules - your own files
+// 3. Third party modules - installed via npm
+
+
+// 6. What is Non-blocking I/O?
+
+// Non blocking I/O allows code to continue execution without waiting
+// It improves performance and mostly used in Node.js 
+
+
+// 7. What is Blocking vs Non-blocking?
+
+// Blocking(Synchronous):
+// Code waits until task finishes
+// It make execution slower
+
+// Non blocking(Asynchronous):
+// Code does not wait
+// It make execution faster
+
+
+// 8. What is process object?
+
+// In Node.js process is a global object that gives information and control over the current running node application
+// It is mostly used for environment variables.
+// process - instance of running program
