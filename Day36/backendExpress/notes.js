05a4f117396b7a3015c28f12d908f7e1169e5341
suba-kannan <subakannan1409@gmail.com>
2026-04-10 10:24:33 +0530

    Day 36 assignment completed

---
+++ Day36/backendExpress/notes.js
@@ -0,0 +1,17 @@
+// 1. what is cors
+
+// cors(cross-origin resource sharing) is a security feature in browsers that controls which website can access your server(API)
+// Without cors client cannot send request to the server.
+// Installation: npm i cors
+
+// 2. what is server?
+
+// A server is a system that provides data or services to other computer over a network.
+// server is a program that listens and response to client request
+// Types:
+// Web server - server website
+// Database server - stors data
+// File server - store files
+
+// 3. what is json()
+// json() is used to parse json data into js object or send data as json.
