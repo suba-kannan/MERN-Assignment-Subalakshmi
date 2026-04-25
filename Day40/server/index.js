ab6ed149b1724b0682dc3f6594b5e04a8c679189
suba-kannan <subakannan1409@gmail.com>
2026-04-25 13:54:39 +0530

    Day 40 assignment completed

--- Day40/server/index.js
+++ Day40/server/index.js
@@ -22,3 +22,32 @@ app.listen(PORT,()=>{
 })
 
 // http://localhost:5000/api/create
+
+// What is Schema?
+
+// Schema in mongoose defines the structure of documents within a mongoDB collection. 
+// It acts as a blueprint that specifies what fields require, default values and validation rules.
+
+
+// what is model?
+
+// Model in mongoose is a wrapper build using a schema that allow it to interact with the mongoDb collection.
+// It provides methods like create, find, update and delete.
+// Models acts like an interface between your application and the database.
+
+
+// what is find()?
+
+// find() is a method used to get data from a mongoDB collection.
+// All documents -no condition
+// Matching documents - with condition
+
+
+// what are all the model based methods?
+
+// Model based methods are functions provided by mongoose to perform CRUD operations.
+// create() - used to add new documents
+// find() - get all or matching documents
+// findById() - get by id
+// findByIdAndUpdate() - get by id abd update that data
+// findOneAndUpdate() - get by id and remove it
